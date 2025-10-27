4c930d30fe079c39cb0abfbc5568601eb29aec24
Ouissal <ouissal.boutouatou@student-cs.fr>
2025-10-27 23:52:02 +0100

    making src a py module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CodeForces-Tags-Prediction",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+    ],
+    author="Ouissal BOUTOUATOU",
+    description="Codeforces problem tag prediction system",
+)
